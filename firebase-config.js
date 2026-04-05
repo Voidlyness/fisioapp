cdba102b6813cc2e3b5d7d5fd64f7f8ef58e600b
Voidlyness <hb.hbandeira@gmail.com>
2026-04-05 19:15:38 +0100

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.FISIO_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDJwHZgH-5PfQwx72FK__KUybgjeprZsYM",
+  authDomain: "physiosite-783b6.firebaseapp.com",
+  projectId: "physiosite-783b6",
+  storageBucket: "physiosite-783b6.firebasestorage.app",
+  messagingSenderId: "858325302248",
+  appId: "1:858325302248:web:b127d907dcd62cc04f825d",
+  measurementId: "G-FKS5MJE7T9"
+};
